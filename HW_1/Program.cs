bed702fbb78e023680d867cf71f09331dd8a629c
Irina <grisha9419@yandex.ru>
2023-03-07 23:11:48 +0300

    add ex 2 and 4

--- HW_1/Program.cs
+++ HW_1/Program.cs
@@ -1,2 +1,33 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//2 Напишите программу, которая на вход принимает 
+//два числа и выдает, какое число большее, а какое меньшее.
+// Console.WriteLine("Введите число"); 
+// int number1 = Convert.ToInt32(Console.ReadLine()); 
+// Console.WriteLine("Введите число"); 
+// int number2 = Convert.ToInt32(Console.ReadLine());
+//  Console.Write("max= ");
+// if(number1>number2) 
+// {
+//     Console.WriteLine(number1);
+// }
+// else
+// {
+//    Console.WriteLine(number2); 
+// }
+
+// 4 Напишите программу, которая принимает на вход 
+//три числа и выдает максимальное из этих чисел.
+// Console.WriteLine("Введите число"); 
+// int number1 = Convert.ToInt32(Console.ReadLine()); 
+// Console.WriteLine("Введите число"); 
+// int number2 = Convert.ToInt32(Console.ReadLine()); 
+// Console.WriteLine("Введите число");
+// int number3 = Convert.ToInt32(Console.ReadLine()); 
+// Console.WriteLine("Введите число");
+// Console.Write("max= ");
+// int max = number1;
+// if (number2>max) max=number2;
+// if (number3>max) max=number3;
+// Console.WriteLine(max);
+
+// 6 Напишите программу, которая на вход принимает число и выдает, 
+// является ли число четным (делится ли оно на два без остатка).
