a9dc0fc56b8a39c10268ca57e2854f5fb00e421c
Irina <grisha9419@yandex.ru>
2023-03-10 22:24:54 +0300

    add task 2

---
+++ HW_2/Program.cs
@@ -0,0 +1,11 @@
+﻿//10 Напишите программу, которая принимает на вход трёхзначное число 
+//и на выходе показывает вторую цифру этого числа.
+
+Console.WriteLine("Введите число"); 
+int number1 = Convert.ToInt32(Console.ReadLine());
+
+int a =  number1/100;
+int d =  number1/10; 
+int result = d-a*10 ;
+ 
+Console.WriteLine(result);
